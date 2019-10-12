8624d190891c07cfdaf038aa91b75d310dcce2ec
Nicu Tofan <nicu.tofan@gmail.com>
2019-10-12 23:49:01 +0300

    Add a simple setup.py file

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+    from setuptools import Command
+except ImportError:
+    from distutils.core import setup
+    from distutils.cmd import Command
+from setuptools.command.develop import develop
+from setuptools import find_packages
+import sys
+sys.setrecursionlimit(5000)
+import glob
+
+cmdclass = {}
+
+
+config = {
+    'name': 'TacoZMQ',
+    'description': 'a friend to friend darknet',
+    'author': 'Scott Powers',
+    'url': 'https://github.com/TheTacoScott/tacozmq/wiki',
+    'download_url': 'https://github.com/TNick/tacozmq',
+    'author_email': '',
+    'version': '0.3.0',
+    'install_requires': [
+        'argparse',
+        'pyzmq',
+    ],
+    'packages': find_packages(),
+    'package_data': {
+        'schema': [],
+    },
+    'scripts': ['tacozmq.py'],
+    'entry_points': {},
+    'extras_require': {
+        'dev': [
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov',
+        ]
+     },
+    'cmdclass': cmdclass
+}
+
+setup(**config)
