ee1266ec5ac2e3c383c5a12a0dfeb543cdd18220
Nicu Tofan <nicu.tofan@gmail.com>
2019-10-13 09:27:31 +0300

    more arguments for the script for basic control

--- tacozmq.py
+++ tacozmq.py
@@ -42,23 +42,87 @@ def make_argument_parser():
     Creates an ArgumentParser to read the options for this script from
     sys.argv.
     """
+    from taco.constants import APP_AUTHOR, APP_NAME
 
     parser = argparse.ArgumentParser(
         description='TacoZMQ: a darknet written in python and zeromq')
     parser.add_argument(
         '--config', default=get_config_file(),
-        dest='configfile',
+        metavar='file', dest='config_file',
         help='specify the location of the config file')
     parser.add_argument(
         "--verbose", default=False,
-        dest="verbose", action="store_true",
-        help="increase output verbosity")
+        action="store_true",
+        help="increase output verbosity; equivalent to --log-level=10")
+    parser.add_argument(
+        "--log-level", default=logging.INFO, type=int,
+        metavar="level", action="store",
+        help="finer control of verbosity (0 to 50); see also --debug")
+    parser.add_argument(
+        '--log-file',
+        metavar="file", action='store',
+        default=os.path.join(
+            user_log_dir(APP_NAME, APP_AUTHOR[0]),
+            '%s.log' % APP_NAME),
+        help='where to save the log; a single - will disable it.')
     parser.add_argument(
         "--version", default=False,
-        dest="version", action="store_true",
+        action="store_true",
         help="print program version and exit")
+
     return parser
 
+
+def setup_logging(args):
+    """
+     Prepares our logging mechanism.
+
+    :param args: Arguments returned by the parser
+    :return: True if all went well, False to exit with error
+    """
+    from taco.constants import APP_NAME, APP_VERSION, APP_STAGE
+    logger = logging.getLogger('')
+
+    # Determine the level of logging.
+    if args.log_level == logging.INFO:
+        log_level = logging.DEBUG if args.verbose else logging.INFO
+    else:
+        try:
+            log_level = int(args.log_level)
+            if (log_level < 0) or (log_level > logging.CRITICAL):
+                raise ValueError
+        except ValueError:
+            print("ERROR! --log-level expects an integer between 1 and %d" %
+                  logging.CRITICAL)
+            return False
+
+    # The format we're going to use in all handlers.
+    fmt = logging.Formatter(
+        "[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d "
+        "[%(funcName)s] %(message)s",
+        '%Y-%m-%d %H:%M:%S')
+
+    # This is the console output.
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(fmt)
+    console_handler.setLevel(log_level)
+    logger.addHandler(console_handler)
+
+    # This is the file output.
+    if len(args.log_file) > 0 and args.log_file != '-':
+        file_path, file_name = os.path.split(args.log_file)
+        if not os.path.isdir(file_path):
+            os.makedirs(file_path)
+        file_handler = logging.FileHandler(args.log_file)
+        file_handler.setFormatter(fmt)
+        file_handler.setLevel(log_level)
+        logger.addHandler(file_handler)
+
+    logger.setLevel(log_level)
+    logger.info(
+        "%s v%.1f %s STARTED", APP_NAME, APP_VERSION, APP_STAGE)
+    logger.debug("logging to %s", args.log_file)
+    return True
 
 
 def main():
@@ -74,6 +138,20 @@ def main():
     args = parser.parse_args()
     args.parser = parser
 
+    # Deal with special cases.
+    if args.version:
+        from taco.constants import APP_NAME, APP_VERSION, APP_STAGE
+        print("%s v%.1f %s" % (APP_NAME, APP_VERSION, APP_STAGE))
+        return 0
+
+    # Handle signals.
+    from taco.globals import properexit
+    signal.signal(signal.SIGINT, properexit)
+
+    # Prepare the logger.
+    if not setup_logging(args):
+        return 1
+
     import taco.routes
     import taco.server
     import taco.clients
@@ -83,29 +161,8 @@ def main():
     import taco.limiter
     import taco.globals
 
-    # Deal with special cases.
-    if args.version:
-        print("%s v%.1f %s" % (
-            taco.constants.APP_NAME, taco.constants.APP_VERSION,
-            taco.constants.APP_STAGE))
-        return 0
+    taco.constants.JSON_SETTINGS_FILENAME = args.config_file
 
-    signal.signal(signal.SIGINT, taco.globals.properexit)
-
-    taco.constants.JSON_SETTINGS_FILENAME = args.configfile
-
-    level = logging.INFO
-    if args.verbose:
-        level = logging.DEBUG
-    logging.basicConfig(
-        level=level,
-        format="[%(levelname)s]\t[%(asctime)s] [%(filename)s:%(lineno)d] "
-               "[%(funcName)s] %(message)s")
-
-
-    logging.info(
-        "%s v%.1f %s STARTED", taco.constants.APP_NAME,
-        taco.constants.APP_VERSION, taco.constants.APP_STAGE)
     taco.settings.Load_Settings()
     taco.crypto.Init_Local_Crypto()
 
@@ -120,11 +177,6 @@ def main():
 
     taco.globals.filesys = taco.filesystem.TacoFilesystemManager()
     taco.globals.filesys.start()
-
-    logging.info(
-        "Starting Local Webserver on %s:%r",
-        taco.globals.settings["Web IP"], taco.globals.settings["Web Port"])
-    logging.info("*** %s Running ***", taco.constants.APP_NAME)
 
     from taco.bottle import run
     run(
